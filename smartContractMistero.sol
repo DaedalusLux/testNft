1c9b5f9377a351cc54a16f628e670ec087279975
Daedalus11 <95046069+DaedalusLux@users.noreply.github.com>
2022-04-12 12:02:53 +0200

    Update smartContractMistero.sol

--- smartContractMistero.sol
+++ smartContractMistero.sol
@@ -47,6 +47,20 @@ contract NFT is ERC721Enumerable, Ownable {
       _safeMint(msg.sender, supply + i);
     }
   }
+  
+     function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
 
 
 
